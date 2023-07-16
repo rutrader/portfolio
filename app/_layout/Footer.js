668a1852b90cc6bdcab9e483af6d22aed06a8944
Ruslan Ishemgulov <ruslan.ishemgulov@gmail.com>
2023-07-16 19:05:04 +0200

    Footer: add more links

--- app/_layout/Footer.js
+++ app/_layout/Footer.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faSquareTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faCamera } from '@fortawesome/pro-regular-svg-icons'
 import { format } from 'date-fns'
 import Link from 'next/link';
 
@@ -22,21 +23,40 @@ export default function Footer() {
 				<div className='copyright'>
 					&copy; {getDate()} ishemgulov.com
 				</div>
-				<div className='social col-md-2 col-8 d-flex justify-content-md between justify-content-around'>
-					<div>
-						<Link href={"https://github.com/rutrader"} target='_blank'>
-							<FontAwesomeIcon icon={faGithub} size='1x' />
-						</Link>
+				<div className='social col-md-6 col-8 d-flex justify-content-md-between justify-content-around'>
+					<div className='col-5'>
+						<div className='link'>
+							<Link href={"https://github.com/rutrader"} target='_blank'>
+								<FontAwesomeIcon icon={faGithub} size='1x' />
+								<span>GitHub</span>
+							</Link>
+						</div>
+						<div className='link'>
+							<Link href={"https://linkedin.com/ruslan.ishemgulov"} target='_blank'>
+								<FontAwesomeIcon icon={faLinkedin} size='1x' />
+								<span>Linkedin</span>
+							</Link>
+						</div>
+						<div className='link'>
+							<Link href={"https://github.com/thebananacorp"} target='_blank'>
+								<FontAwesomeIcon icon={faSquareTwitter} size='1x' />
+								<span>Twitter</span>
+							</Link>
+						</div>
 					</div>
-					<div>
-						<Link href={"https://linkedin.com/ruslan.ishemgulov"} target='_blank'>
-							<FontAwesomeIcon icon={faLinkedin} size='1x' />
-						</Link>
-					</div>
-					<div>
-						<Link href={"https://github.com/thebananacorp"} target='_blank'>
-							<FontAwesomeIcon icon={faSquareTwitter} size='1x' />
-						</Link>
+					<div className='col-5'>
+						<div className='link'>
+							<Link href={"https://instagram.com/ruslan.ishemgulov"} target='_blank'>
+								<FontAwesomeIcon icon={faInstagram} size='1x' />
+								<span>Instagram</span>
+							</Link>
+						</div>
+						<div className='link'>
+							<Link href={"https://instagram.com/ruslan.ishemgulov"} target='_blank'>
+								<FontAwesomeIcon icon={faCamera} size='1x' />
+								<span>Photo portfolio</span>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
