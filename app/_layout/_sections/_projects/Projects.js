71417c8c3c4890f742a506a43d6271d3dbb6046a
Ruslan Ishemgulov <ruslan.ishemgulov@gmail.com>
2023-07-16 18:29:53 +0200

    Layout: update projects section
    * use carousel
    * fix projects descriptions
    * add ewijobs project

--- app/_layout/_sections/_projects/Projects.js
+++ app/_layout/_sections/_projects/Projects.js
@@ -1,69 +1,86 @@
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRectangleTerminal } from '@fortawesome/pro-thin-svg-icons'
+import { faRectangleTerminal, faArrowUpRightFromSquare } from '@fortawesome/pro-thin-svg-icons'
 
 import czechstatsImage from '../../../../public/images/projects/czechstats.png'
 import workmarketImage from '../../../../public/images/projects/workmarket.png'
+import ewijobsImage from '../../../../public/images/projects/ewijobs.png'
+import { Carousel } from './carousel'
+import Link from 'next/link'
 
 export default function Projects() {
+
+	const projects = [
+		{
+			title: 'CzechStats',
+			// description: 'Czech Republic economic indicators in one place. \
+			// Our website provides a comprehensive overview of the Czech Republic economy, \
+			// including key economic indicators, financial data, \
+			// monetary and fiscal policy, and government statistics.',
+			description: 'This side project offered a comprehensive overview of the Czech Republic economy, showcasing key indicators, financial data, and government stats, providing valuable insights into the economic landscape.',
+			image: czechstatsImage,
+			stacks: 'React, NodeJS, Context API',
+			link: 'https://czechstats.eu/'
+		},
+		{
+			title: 'Workmarket.eu',
+			description: 'A job portal connecting non-European residents with employment opportunities in EU countries, helping companies fill open positions with international talent, fostering cross-border collaboration and diverse workforce.',
+			image: workmarketImage,
+			stacks: 'PHP, Symfony, PostgreSQL',
+			link: 'https://workmarket.eu/'
+		},
+		{
+			title: 'Ewijobs.cz',
+			description: ' My latest project, Ewijobs, is a simplified job portal featuring job ads and a user-friendly reply form, streamlining the job-seeking process for candidates and employers, making hiring more efficient.',
+			image: ewijobsImage,
+			stacks: 'Next.js, Netlify, Bootstrap, MongoDB',
+			link: 'https://ewijobs.cz/'
+		},
+	]
+
 	return (
 		<>
 			<div className="projects d-flex align-items-lg-start align-items-center flex-column">
 				<h2 className="gradient">Last projects</h2>
 
-				<div className="align-items-center align-items-lg-start d-flex flex-column flex-lg-row items justify-content-between">
-					<div className="item col-lg-4 col-10 d-flex flex-column justify-content-between">
-						<Image
-							src={czechstatsImage}
-							alt="czechstats.eu screenshot"
-						/>
-						<div className='divider'></div>
-						<h3>CzechStats</h3>
-						<div className='py-2 description'>
-							<p>
-								Czech Republic economic indicators in one place.
-								Our website provides a comprehensive overview of the Czech Republic economy,
-								including key economic indicators, financial data,
-								monetary and fiscal policy, and government statistics.
-							</p>
-						</div>
+				<div className="items align-items-center align-items-lg-start d-flex flex-column flex-lg-row items justify-content-between">
 
-						<div className='py-2'>
-							<h6>Stack</h6>
-							<div className='d-flex justify-content-between align-items-center'>
-								<FontAwesomeIcon icon={faRectangleTerminal} />
-								<span className='fw-light'>
-									React, NodeJS, Context API
-								</span>
-							</div>
-						</div>
-					</div>
-					
-					<div className="item col-lg-4 col-10 d-flex flex-column justify-content-between">
-						<Image
-							src={workmarketImage}
-							alt="workmarket.eu screenshot"
-						/>
-						<div className='divider'></div>
-						<h3>Workmarket.eu</h3>
-						<div className='py-2 description'>
-							<p>
-							Workmarket.eu - Europe's #1 job site for multilingual jobseekers. 
-							Vacancies and recruitment in Europe.
-							</p>
-						</div>
+					{projects.map((project, key) => {
+						return (
+							<div key={`project-item-${key}`} className="item col-lg-3 col-10 d-flex flex-column justify-content-between">
+								<Image
+									src={project.image}
+									alt={`${project.title} screenshot`}
+								/>
+								<div className='divider'></div>
+								<h3>
+									{project.title}
 
-						<div className='py-2'>
-							<h6>Stack</h6>
-							<div className='d-flex justify-content-between align-items-center'>
-								<FontAwesomeIcon icon={faRectangleTerminal} />
-								<span className='fw-light'>
-									PHP, Symfony, PostgreSQL
-								</span>
+									<Link href={project.link} target='_blank'>
+										<FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2xs" className='mx-3' />
+									</Link>
+								</h3>
+								<div className='py-2 description'>
+									<p>
+										{project.description}
+									</p>
+								</div>
+
+								<div className='py-2'>
+									<h6>Stack</h6>
+									<div className='d-flex justify-content-between align-items-center'>
+										<FontAwesomeIcon icon={faRectangleTerminal} />
+										<span className='fw-light'>
+											{project.stacks}
+										</span>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
+						)
+					})}
 				</div>
+
+				<Carousel projects={projects} />
 			</div>
 		</>
 	)
---
+++ app/_layout/_sections/_projects/carousel.js
@@ -0,0 +1,75 @@
+'use client'
+
+import useEmblaCarousel from 'embla-carousel-react'
+import Image from 'next/image';
+import { useCallback } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareChevronRight, faSquareChevronLeft, faRectangleTerminal, faArrowUpRightFromSquare } from '@fortawesome/pro-thin-svg-icons'
+import Link from 'next/link';
+
+export const Carousel = (props) => {
+
+	const { projects } = props;
+
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev()
+	}, [emblaApi])
+
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext()
+	}, [emblaApi])
+
+	return (
+		<div className="embla overflow-hidden" ref={emblaRef}>
+			<div className="embla__container">
+
+				{projects.map((project, key) => {
+					return (
+						<div key={`embla-stack-item-${key}`} className="embla__slide" style={{ flex: '0 0 100%', minWidth: 0 }}>
+
+							<div key={`project-item-${key}`} className="item col-lg-3 col-10 d-flex flex-column justify-content-between">
+								<Image
+									src={project.image}
+									alt={`${project.title} screenshot`}
+								/>
+								<div className='divider'></div>
+								<h3>
+									{project.title}
+
+									<Link href={project.link} target='_blank'>
+										<FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2xs" className='mx-3' />
+									</Link>
+								</h3>
+								<div className='py-2 description'>
+									<p>
+										{project.description}
+									</p>
+								</div>
+
+								<div className='py-2'>
+									<h6>Stack</h6>
+									<div className='d-flex justify-content-lg-between justify-content-start align-items-center'>
+										<FontAwesomeIcon icon={faRectangleTerminal} />
+										<span className='fw-light mx-3'>
+											{project.stacks}
+										</span>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					)
+				})}
+
+			</div>
+
+			<div className='col-6 d-flex justify-content-around mx-auto'>
+				<FontAwesomeIcon icon={faSquareChevronLeft} onClick={scrollPrev} size='2x' />
+				<FontAwesomeIcon icon={faSquareChevronRight} onClick={scrollNext} size='2x' />
+
+			</div>
+		</div>
+	)
+}
